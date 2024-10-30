a11e005f7eabbbcb064fffdc2217c8c339b51069
HOAn0 <ttql1211@gmail.com>
2024-10-30 13:13:27 +0700

    Initial commit

---
+++ WebVali-main/WebVali/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebVali.Models;
+
+namespace WebVali.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly QlbanVaLiContext _context;
+        public ProductController(QlbanVaLiContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Product()
+        {
+            var products = _context.TChiTietSanPhams
+            .Include(p => p.MaKichThuocNavigation)
+            .Include(p => p.MaMauSacNavigation)
+            .ToList();
+
+            return View(products);
+        }
+    }
+}
